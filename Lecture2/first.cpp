882f13077fbdc3d8b2bc17e16703fe84cbfaf408
Mehak Garg <missgunnu1@gmail.com>
2025-12-25 20:50:29 +0530

    DSA

---
+++ Lecture2/first.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  // int a;
+  // cout << "Enter your number: ";
+  // cin >> a;
+  // cout << "Your number is: " << a;
+
+  int a,b;
+  cout << "Enter the first number: ";
+  cin >> a;
+  cout << "Enter the second number: ";
+  cin >> b;
+  cout << "Sum of the numbers: " << a+b;
+
+}
