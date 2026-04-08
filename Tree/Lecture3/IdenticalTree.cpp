8b6aba82089b564264a717db1d002494fd2085cf
Mehak Garg <missgunnu1@gmail.com>
2026-04-08 23:55:32 +0530

    Identical Tree

---
+++ Tree/Lecture3/IdenticalTree.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node *left, *right;
+  Node(int val){
+    data = val;
+    left = right = NULL;
+  }
+};
+
+bool identical(Node *r1, Node *r2){
+  if(!r1 && !r2)
+  return 1;
+
+  if((!r1&&r2) || (r1&&!r2))
+  return 0;
+
+  if(r1->data!=r2->data)
+  return 0;
+
+  return identical(r1->left,r2->left) || identical(r1->right,r2->right);
+}
